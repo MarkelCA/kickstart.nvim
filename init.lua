9074d83aa8bf8e0bc0cae53bfa90d9b427b906ca
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-05-18 19:23:18 +0200

    Some refactorings

--- init.lua
+++ init.lua
@@ -30,15 +30,7 @@ require('lazy').setup({
   { 'github/copilot.vim' },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    opts = {},
   },
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
